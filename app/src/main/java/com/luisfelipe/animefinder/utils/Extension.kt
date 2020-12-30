6f4a4b46f20e79ec948dadafee7c2d5a35183842
lf <luisfelipecf94@gmail.com>
2020-12-30 19:43:39 -0300

    first commit

---
+++ app/src/main/java/com/luisfelipe/animefinder/utils/Extension.kt
@@ -0,0 +1,36 @@
+package com.luisfelipe.animefinder.utils
+
+import android.app.Activity
+import android.view.View
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import com.squareup.picasso.Picasso
+
+fun Fragment.toast(message: String, duration: Int = Toast.LENGTH_SHORT) {
+    Toast.makeText(this.context, message, duration).show()
+}
+
+fun Activity.toast(message: String, duration: Int = Toast.LENGTH_SHORT) {
+    Toast.makeText(this, message, duration).show()
+}
+
+fun Fragment.toast(messageId: Int, duration: Int = Toast.LENGTH_SHORT) {
+    Toast.makeText(this.context, getString(messageId), duration).show()
+}
+
+fun Activity.toast(messageId: Int, duration: Int = Toast.LENGTH_SHORT) {
+    Toast.makeText(this, getString(messageId), duration).show()
+}
+
+fun View.visible() {
+    visibility = View.VISIBLE
+}
+
+fun View.gone() {
+    visibility = View.GONE
+}
+
+fun ImageView.load(imageUrl: String) {
+    Picasso.get().load(imageUrl).into(this)
+}
